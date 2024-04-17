34dc58be4f8b0fe0fe15575f9f9fbf4428bf5106
Eli Kinkade <ekinkade21@gmail.com>
2024-04-17 16:56:35 -0400

    3.5.2 start over

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,6 +1,5 @@
 import Signin from "../../Users/Signin";
 import Profile from "../../Users/Profile";
-import UserTable from "../../Users/Table";
 import { Routes, Route, Navigate } from "react-router-dom";
 export default function Account() {
   return (
@@ -9,7 +8,6 @@ export default function Account() {
         <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
         <Route path="/Signin" element={<Signin />} />
         <Route path="/Profile" element={<Profile />} />
-        <Route path="/Admin/Users" element={<UserTable />} />
       </Routes>
     </div>
   );
